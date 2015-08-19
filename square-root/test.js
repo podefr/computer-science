742c973472c8f284b4b91de41ccef28cc6b295e7
Olivier Scherrer <pode.fr@gmail.com>
2015-08-18 23:59:01 -0400

    add max call stack error example

--- square-root/test.js
+++ square-root/test.js
@@ -1,12 +1,20 @@
 var squareRoot = require("./index");
 
-[654867897, 36].forEach(executeSqrtForNumber);
+[654867897, 36, 2, 65464324897].forEach(executeSqrtForNumber);
 
 function executeSqrtForNumber(number) {
     Object.keys(squareRoot).forEach(function (func) {
         console.time(func);
-        var result = squareRoot[func](number);
-        console.log("Sqrt(", number, ")  = ", result[0], "in",  result[1], "iterations with", func, "method");
+        var result;
+
+        try {
+            result = squareRoot[func](number);
+            console.log("Sqrt(", number, ")  = ", result[0], "in",  result[1], "iterations with", func, "method");
+
+        } catch (err) {
+            console.log(err);
+        }
+
         console.timeEnd(func);
         console.log("\n");
     });
