be11d8674cb305390f1e4d8c60d5db71fbab0fdd
Olivier Scherrer <pode.fr@gmail.com>
2015-08-22 18:57:44 -0400

    add palindrom test

--- string/text.js
+++ string/text.js
@@ -1,9 +1,10 @@
 "use strict";
 
 function Text(text) {
+    var textLength = text.length;
+
     this.search = function search(searchString) {
         var searchStringLength = searchString.length;
-        var textLength = text.length;
         var positions = [];
 
         for (var i = 0; i < textLength; i++) {
@@ -19,7 +20,47 @@ function Text(text) {
     };
 
     this.highlight = function mark(searchString) {
-        return text.split(searchString).join("<em>" +  searchString + "</em>");
+        return text.replace(new RegExp(searchString, "g"), "<em>" +  searchString + "</em>");
+    };
+
+    /**
+     * Finds if it's a palindrom using temp arrays and strings
+     * @returns {boolean}
+     */
+    this.isPalindrom = function isPalindrom() {
+        var textToTest = text.replace(/ /g, "").toLowerCase();
+
+        return textToTest
+                .split('')
+                .reverse()
+                .join('') === textToTest;
+    };
+
+    /**
+     * Finds if it's a palindrom in place with 0(n) time without extra data structures
+     * @returns {boolean}
+     */
+    this.isPalindromInPlace = function isPalindromInPlace() {
+        var leftCursor = 0;
+        var rightCursor = text.length - 1;
+        var middle = Math.floor(rightCursor / 2);
+        var same = true;
+
+        while (same && leftCursor < middle) {
+            while (text[leftCursor] == " ") {
+                leftCursor++;
+            }
+
+            while (text[rightCursor] == " ") {
+                rightCursor--;
+            }
+
+            same = text[leftCursor].toLowerCase() == text[rightCursor].toLowerCase();
+            leftCursor++;
+            rightCursor--;
+        }
+
+        return same;
     };
 }
 
@@ -31,3 +72,8 @@ var text = new Text("adipisci velit, sed quia non numquam eius modi tempora inci
 
 console.log(text.search("vel"));
 console.log(text.highlight("vel"));
+
+var palindrom = new Text("Engage le jeu que je le gagne");
+
+console.log(palindrom.isPalindrom());
+console.log(palindrom.isPalindromInPlace());
