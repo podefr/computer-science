32df783db3e81f2c7e3247f1963ec2392ed6e23c
Olivier Scherrer <pode.fr@gmail.com>
2015-08-22 17:56:06 -0400

    fix new array example

--- merge/merge-sorted.js
+++ merge/merge-sorted.js
@@ -27,13 +27,13 @@ function mergeSorted(array1, array2) {
     var newArray = [];
 
     for (var i = 0; i < length; i++) {
-        if (array1[0] > array2[0]) {
-            newArray.push(array2.shift());
-        } else if (array2[0] < array1[0]) {
+        if (array1[0] < array2[0]) {
             newArray.push(array1.shift());
-        } else if (array1[0] === undefined) {
+        } else if (array2[0] < array1[0]) {
             newArray.push(array2.shift());
-        } else if (array2[0] === undefined) {
+        } else if (!array1.length) {
+            newArray.push(array2.shift());
+        } else if (!array2.length) {
             newArray.push(array1.shift());
         }
     }
@@ -50,6 +50,7 @@ console.log(mergeSortedInPlace([0, 1, 3, 5, 8], [2, 4, 6, 7]));
 console.log(mergeSortedInPlace([0, 1], [2, 3, 4, 5, 6, 7]));
 
 console.log("new array");
-//console.log(mergeSorted([0, 1, 3, 5], [2, 4, 6]));
-//console.log(mergeSorted([-1, 1, 3, 5], [-2, 0, 2, 4, 6]));
-//console.log(mergeSorted([0, 1, 3, 5, 8], [2, 4, 6, 7]));
+
+console.log(mergeSorted([0, 1, 3, 5], [2, 4, 6]));
+console.log(mergeSorted([-1, 1, 3, 5], [-2, 0, 2, 4, 6]));
+console.log(mergeSorted([0, 1, 3, 5, 8], [2, 4, 6, 7]));
