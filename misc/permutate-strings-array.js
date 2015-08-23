64e95ecf89c4d11aa2108161c87dc6003a1771d3
Olivier Scherrer <pode.fr@gmail.com>
2015-08-23 00:13:12 -0400

    add string permutation

---
+++ misc/permutate-strings-array.js
@@ -0,0 +1,28 @@
+/**
+ * Same here, still trying to understand this one
+ * @param array
+ * @returns {*}
+ */
+
+function permutate(array) {
+    var permutations = [];
+
+    function doPermute(words, permutation) {
+        permutation = permutation || "";
+
+        if (!words.length) {
+            permutations.push(permutation);
+            return;
+        }
+
+        words[0].split('').forEach(function (char) {
+            doPermute(words.slice(1), permutation + char);
+        });
+
+        return permutations;
+    }
+
+    return doPermute(array);
+}
+
+console.log(permutate(["red", "fox", "super"]).length);
