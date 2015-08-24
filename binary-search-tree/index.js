e55fa2c30e17c3d8af051d3a73b450e2c60d14e7
Olivier Scherrer <pode.fr@gmail.com>
2015-08-23 22:30:47 -0400

    Add a bunch of new functions:
    - addArray to add an array of sorted values in a balanced way, works in O(n log(n))
    - flatten to flatten an existing BST if depth gets too high, works in O(n)
    - depth to get the current depth, works in O(n)
    - erase to empty the BST

--- binary-search-tree/index.js
+++ binary-search-tree/index.js
@@ -4,6 +4,27 @@ module.exports = function BST() {
 
     var _head;
 
+    this.addArray = function addArray(array) {
+        var pivot;
+
+        if (array.length) {
+            pivot = Math.floor(array.length / 2);
+            this.insert(array[pivot]);
+            this.addArray(array.slice(0, pivot));
+            this.addArray(array.slice(pivot + 1));
+        }
+    };
+
+    this.flatten = function flatten() {
+        var array = this.toArray();
+        this.erase();
+        this.addArray(array);
+    };
+
+    this.erase = function erase() {
+        _head = null;
+    };
+
     this.insert = function insert(item) {
         if (!_head) {
             _head = createNode(item);
@@ -13,6 +34,7 @@ module.exports = function BST() {
     };
 
     this.delete = function delete_(item) {
+
     };
 
     this.forEach = function forEach(func, scope) {
@@ -29,12 +51,21 @@ module.exports = function BST() {
         return array;
     };
 
-    this.size = function size() {
+    this.length = function length() {
         var count = 0;
         traverse(function () {
             count++;
         });
         return count;
+    };
+
+    this.depth = function depth() {
+        var _bstDepth = 0;
+        traverse(function (node, nodeDepth) {
+            _bstDepth = nodeDepth > _bstDepth ? nodeDepth : _bstDepth;
+        });
+
+        return _bstDepth;
     };
 
     this.getHead = function getHead() {
@@ -50,22 +81,24 @@ module.exports = function BST() {
     }
 
     function traverse(callback) {
-        function inOrder(current) {
+        function inOrder(current, depth) {
+            depth = depth || 0;
+
             if (current.left) {
-                inOrder(current.left);
+                inOrder(current.left, depth + 1);
             }
 
-            callback(current);
+            callback(current, depth);
 
             if (current.right) {
-                inOrder(current.right);
+                inOrder(current.right, depth + 1);
             }
         }
         inOrder(_head);
     }
 
     function addToParentNode(item, current) {
-        current = current || _head;
+        current = current || _head;
 
         if (item > current.item) {
             if (!current.right) {
--- binary-search-tree/test.js
+++ binary-search-tree/test.js
@@ -2,21 +2,37 @@ var BST = require("./index");
 
 var bst = new BST;
 
-bst.insert(20);
-bst.insert(20);
-bst.insert(10);
-bst.insert(5);
-bst.insert(30);
-bst.insert(30);
-bst.insert(15);
+bst.addArray([5, 10, 15, 20, 25, 30]);
 
 
 console.log(bst.getHead());
 
-console.log(bst.toArray());
+console.log("array", bst.toArray());
 
-console.log(bst.size());
+console.log(bst.length());
 
 bst.forEach(function (item) {
    console.log(item);
 });
+
+console.log("erase ----");
+
+bst.erase();
+
+
+console.log("insert items unbalanced");
+
+bst.insert(5);
+bst.insert(10);
+bst.insert(15);
+bst.insert(20);
+bst.insert(25);
+bst.insert(30);
+
+console.log("depth is", bst.depth());
+console.log(bst.getHead());
+
+bst.flatten();
+
+console.log("new depth", bst.depth());
+console.log(bst.getHead());
