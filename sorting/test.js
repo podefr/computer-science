fc894ffe292e927cfe93bb7d936b05f60ced7663
Olivier Scherrer <pode.fr@gmail.com>
2015-08-25 14:05:55 -0400

    add bubblesort

--- sorting/test.js
+++ sorting/test.js
@@ -5,3 +5,7 @@ console.log(quicksort([-3, 5, 10, 3, 6, 8]));
 var insertionsort = require("./insertionsort");
 
 console.log(insertionsort([-10, 15, -3, 5, 10, 3, 6, 8]));
+
+var bubblesort = require("./bubblesort");
+
+console.log(bubblesort([9, -10, 15, -3, 5, 10, 3, 6, 8]));
