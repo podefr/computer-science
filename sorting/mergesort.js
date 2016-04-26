2888212d63d0b073465ec0013906143747349625
Olivier Scherrer <pode.fr@gmail.com>
2016-04-26 00:34:57 -0400

    Fix and simplify merge sort

--- sorting/mergesort.js
+++ sorting/mergesort.js
@@ -1,10 +1,9 @@
 "use strict";
 
 function merge(array1, array2) {
-    var newLength = array1.length + array2.length;
     var newArray = [];
 
-    while (newLength--) {
+    while (array1.length || array2.length) {
         if (array1[0] <= array2[0] || !array2.length) {
             newArray.push(array1.shift());
         } else {
@@ -15,13 +14,13 @@ function merge(array1, array2) {
 }
 
 function mergeSort(array) {
-    var array1;
+    var arrayLeft;
 
     if (array.length <= 1) {
         return array;
     } else {
-        array1 = array.splice(0, Math.floor(array.length / 2));
-        return merge(mergeSort(array1), mergeSort(array));
+        arrayLeft = array.splice(0, Math.floor(array.length / 2));
+        return merge(mergeSort(arrayLeft), mergeSort(array));
     }
 
 }
