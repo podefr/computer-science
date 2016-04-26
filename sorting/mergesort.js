fa95b9d4018330c78ba435dcabb3453d9e17167d
Olivier Scherrer <pode.fr@gmail.com>
2016-04-25 23:29:51 -0400

    Fix and simplify mergesort

--- sorting/mergesort.js
+++ sorting/mergesort.js
@@ -1,14 +1,7 @@
 "use strict";
 
-function sortSimpleArray(array) {
-    if (array[0] <= array[1]) {
-        return array;
-    } else {
-        return array.reverse();
-    }
-}
-
 function merge(array1, array2) {
+
     var newLength = array1.length + array2.length;
     var newArray = [];
 
@@ -19,9 +12,12 @@ function merge(array1, array2) {
             if (array2.length) {
                 newArray.push(array2.shift());
             }
+
+            if (!array2.length && array1.length) {
+                newArray.push(array1.shift());
+            }
         }
     }
-
     return newArray;
 }
 
@@ -30,17 +26,12 @@ function mergeSort(array) {
 
     if (array.length <= 1) {
         return array;
-    }
-
-    if (array.length === 2) {
-        return sortSimpleArray(array);
     } else {
         array1 = array.splice(0, Math.floor(array.length / 2));
         return merge(mergeSort(array1), mergeSort(array));
     }
 
 }
-
 
 console.log(mergeSort("MERGESORT".split("")));
 console.log(mergeSort([-10, 3.4, 1, 6.5, 12, -3.14]));
