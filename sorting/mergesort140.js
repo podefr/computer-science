fb6f360476e04c94d034ac242cb4408ead5b767a
Olivier Scherrer <pode.fr@gmail.com>
2016-04-26 20:21:18 -0400

    golfed merge sort down to 140 bytes, add annotations

--- sorting/mergesort140.js
+++ sorting/mergesort140.js
@@ -1,18 +1,38 @@
-// Current status: 166 bytes (:()
+// e: the original array to sort
+// r: will be used to store half of the original array
+// g: will store the length of the original array
+// s: will store the new sorted array that's returned
+function annotatedM(e,r,g,s) {
+    g = e.length
 
-function m(a, b, n) {
-    while (a.length || b.length) n.push((a[0] < b[0] || !b.length ? a : b).shift());
-    return n;
+    // If the length is one or less, the array is sorted, we can return it
+    if (g<2) return e
+
+    // Otherwise, split the array in halves by:
+    // splice(~~g/2) effectively does splice(Math.floor(lengthOriginalArray / 2))
+    // and returns half of the array that we then recursively merge sort by calling the merge function on
+    // and we reuse the variable of the original array (e) to call the merge sort function on the rest of the array, the other half.
+    // then we loop for as many items as they were to in the original array, to merge the two resulting, sorted arrays into the new array
+    for (r=m(e.splice(~~g/2)), e=m(e), s=[]; g--;)
+        // here we merge two sorted arrays
+        // by taking the smallest value at position 0 of each array.
+        // Example: merging [0,1,3] and [2,4], we first take 0, push it onto the new array, and remove it from the original one:
+        // s === [0], e === [1,3], r=== [2,4]
+        // then s === [0,1], e === [3], r === [2,4]
+        // then s === [0,1,2], e === [3], r === [2,4]
+        // then s === [0,1,2,3], e ===[], r===[4]
+        // and s === [0,1,2,3,4], e===[], r===[]
+        // based on how we split, we  may have more items in the left array or the right array,
+        // so the condition !r.length ensures that if one array is empty (r), we automatically pick from the other one. (e)
+        s.push((e[0] < r[0] | !r.length ? e : r).shift())
+
+    // Finally, we return the merged arrays
+    return s
 }
 
-function s(a, l) {
-    l = a.length;
-    return l < 2 ? a : m(s(a.splice(-~~l / 2)), s(a), []);
-}
+function m(e,r,g,s){g=e.length;if(g<2)return e;for(r=m(e.splice(~~g/2)),e=m(e),s=[];g--;)s.push((e[0]<r[0]|!r.length?e:r).shift());return s}
 
-console.log(s("MERGESORT".split("")));
-console.log(s([-10, 3.4, 1, 6.5, 12, -3.14]));
-
-// the 0 here shouldn't break the while loop in the merge function
-console.log(s([-10, 3.4, 1, 6.5, 0, -3.14]));
+console.log(m("MERGESORT".split("")));
+console.log(m([-10, 3.4, 1, 6.5, 12, -3.14]));
+console.log(m([-10, 3.4, 1, 6.5, 0, -3.14]));
 
