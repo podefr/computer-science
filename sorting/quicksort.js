ce28345f198344fc3266ad9f1e34dfe3b7e8f908
Olivier Scherrer <pode.fr@gmail.com>
2016-04-27 23:26:55 -0400

    Add own quick sort with golfed down version

--- sorting/quicksort.js
+++ sorting/quicksort.js
@@ -1,56 +1,32 @@
 "use strict";
 
-// QUICKSORT copied from http://www.nczonline.net/blog/2012/11/27/computer-science-in-javascript-quicksort/
-// as a learning exercise.
+function quickSort(array) {
+    if (array.length < 2) {
+        return array;
+    }
 
-function swap(items, left, right) {
-    var temp = items[left];
-    items[left] = items[right];
-    items[right] = temp;
+    var pivot = array.shift();
+    var left = [];
+    var right = [];
+    var current;
+
+    while (array.length) {
+        current = array.shift();
+        if (current < pivot) {
+            left.push(current);
+        } else {
+            right.push(current);
+        }
+    }
+
+    return [].concat(quickSort(left), pivot, quickSort(right));
 }
 
-function partition(items, left, right) {
-    var pivot = items[Math.floor((left + right) / 2)];
-    var l = left;
-    var r = right;
+// golfed ES5, 139 bytes
+function q(u,i,c,k,s){i=u.length;if(i<2)return u;c=u.shift();k=[];s=[];while(--i)(u[0]<c?k:s).push(u.shift());return[].concat(q(k),c,q(s))}
 
-    while (l <= r) {
-        while (items[l] < pivot) {
-            l++;
-        }
 
-        while(items[r] > pivot) {
-            r--;
-        }
-
-        if (l <= r) {
-            swap(items, l, r);
-            l++;
-            r--;
-        }
-    }
-
-    return l;
-}
-
-module.exports = function quicksort(items, left, right) {
-
-    if (items.length <= 1) {
-        return;
-    }
-
-    left = left || 0;
-    right = right || items.length - 1;
-
-    var index = partition(items, left, right);
-
-    if (left < index - 1) {
-        quicksort(items, left, index - 1);
-    }
-
-    if (right > index) {
-        quicksort(items, index, right);
-    }
-
-    return items;
-};
+// tests
+console.log(q("QUICKSORT".split("")));
+console.log(q([-10, 3.4, 1, 6.5, 12, -3.14]));
+console.log(q([-10, 3.4, 1, 6.5, 0, -3.14]));
