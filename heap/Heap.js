5707b17d46937e2b0228d8d04f07fd64e09b4208
Olivier Scherrer <pode.fr@gmail.com>
2017-03-27 23:32:01 -0400

    Add min heap and support for custom comparison

--- heap/Heap.js
+++ heap/Heap.js
@@ -1,15 +1,10 @@
-// quick implementation of a MaxHeap
-// will improve this to:
-// - support min heap
-// - use clever algorithm to create a heap from an array
-
-function Heap() {
-    const heap = [];
+function Heap(comparator = (a, b) => a > b) {
+    let heap = [];
 
     this.push = function push(value) {
         heap.push(value);
 
-        siftUp(heap.length -1);
+        siftUp(heap.length - 1);
     };
 
     this.pop = function pop() {
@@ -21,7 +16,7 @@ function Heap() {
         const newRoot = heap.pop();
         heap.unshift(newRoot);
 
-        siftDown(0);
+        siftDown();
 
         return root;
     };
@@ -33,40 +28,66 @@ function Heap() {
     this.replace = function replace(value) {
         heap[0] = value;
 
-        siftDown(0);
+        siftDown();
     };
 
-    this.reset = function reset() {
+    this.init = function init(resetValues) {
+        // using floyds method
+        let length = resetValues.length;
 
+        heap.length = 0;
+
+        while (length--) {
+            heap.unshift(resetValues[length]);
+            siftDown();
+        }
+    };
+
+    this.size = function () {
+        return heap.length;
     };
 
     this.dump = function dump() {
         return heap.slice(0);
     };
 
+    this.toSortedArray = function toSortedArray() {
+        const sortedArray = [];
+        let nextValue;
+
+        while (nextValue = this.pop()) {
+            sortedArray.push(nextValue);
+        }
+
+        return sortedArray
+    };
+
     function siftUp(index) {
         if (!isRoot(index)) {
             const parentIndex = getParentIndex(index);
-            if (heap[index] > heap[parentIndex]) {
+            if (comparator(heap[index], heap[parentIndex])) {
                 swap(parentIndex, index);
                 siftUp(parentIndex);
             }
         }
     }
 
-    function siftDown(index) {
+    function siftDown(index = 0) {
         const itemValue = heap[index];
         const leftChildIndex = getLeftChildIndex(index);
         const rightChildIndex = getRightChildIndex(index);
+
+        if (leftChildIndex >= heap.length || rightChildIndex >= heap.length) {
+            return;
+        }
+
         const leftChildValue = heap[leftChildIndex];
         const rightChildValue = heap[rightChildIndex];
 
-        if (leftChildValue > itemValue && leftChildValue > rightChildValue) {
+        if (comparator(leftChildValue, itemValue) && comparator(leftChildValue, rightChildValue)) {
             swap(index, leftChildIndex);
             siftDown(leftChildIndex);
-        }
-
-        if (rightChildValue > itemValue && rightChildValue > leftChildValue) {
+        } else if (comparator(rightChildValue, itemValue) && comparator(rightChildValue, leftChildValue)) {
             swap(index, rightChildIndex);
             siftDown(rightChildIndex);
         }
@@ -95,4 +116,8 @@ function Heap() {
     }
 }
 
-module.exports = Heap;
+module.exports = {
+    Heap: Heap,
+    maxHeap: (a, b) => a > b,
+    minHeap: (a, b) => a < b
+};
--- sorting/heapsort.js
+++ sorting/heapsort.js
@@ -1,11 +1,35 @@
-const Heap = require('../heap/Heap');
+const { Heap, maxHeap, minHeap } = require('../heap/Heap');
 
-function heapSort(array) {
-    const heap = new Heap();
-
-    array.forEach(x => heap.push(x));
-
-    return array.map(() => heap.pop());
+function maxHeapSort(array) {
+    const heap = new Heap(maxHeap);
+    heap.init(array);
+    return heap.toSortedArray();
 }
 
-console.log(heapSort([10, 3, 4, 8, 2, 9, 7, 1, 6, 5]));
+function minHeapSort(array) {
+    const heap = new Heap(minHeap);
+    heap.init(array);
+    return heap.toSortedArray();
+}
+
+function funkyHeapSort(array) {
+    const heap = new Heap((a, b) => a.age > b.age);
+    heap.init(array);
+    return heap.toSortedArray();
+}
+
+console.log(maxHeapSort([10, 3, 4, 8, 2, 9, 7, 1, 6, 5]));
+console.log(minHeapSort([10, 3, 4, 8, 2, 9, 7, 1, 6, 5]));
+
+console.log(funkyHeapSort([
+    {age: 10, name: 10},
+    {age: 3, name: 3},
+    {age: 4, name: 4},
+    {age: 8, name: 8},
+    {age: 2, name: 2},
+    {age: 9, name: 9},
+    {age: 7, name: 7},
+    {age: 1, name: 1},
+    {age: 6, name: 6},
+    {age: 5, name: 5},
+]).map(x => x.name));
